fcefd1c02aeb37b29d0f7f030d5161f8bb1d52b8
Kevin Mathews <kev.math@yahoo.com>
2021-03-28 21:49:13 -0700

    good progress on portfolio site

---
+++ js/script.js
@@ -0,0 +1,53 @@
+$(() => {
+
+$('#slides').superslides({ 
+    animation: 'fade',
+    play: 4000,
+    pagination: false,
+    animation_easing: 'swing'
+})
+
+let typed = new Typed(".typed", {
+    strings: ["Full Stack Software Engineer", "Web Developer"],
+    typeSpeed: 80,
+    loop: true,
+    fadeOut: true,
+    fadeOutDelay: 1500,
+    startDelay: 1000,
+    showCursor: false
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 4,
+    responsive:{
+        0:{
+            items:1
+        },
+        480:{
+            items:2
+        },
+        768:{
+            items:3
+        },
+        938:{
+            items:4
+        }
+    }
+})
+
+$(function() {
+    $('.chart').easyPieChart({
+        easing: 'easeInOut',
+        barColor: '#ffffff',
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function(from, to, percent) {
+            $(this.el).find('.percent').text(Math.round(percent));
+        }
+    });
+});
+
+});
