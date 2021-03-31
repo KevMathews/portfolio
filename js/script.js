f9d96ea7f35aa3553ce8dd616381f70e0031bbb7
Kevin Mathews <kev.math@yahoo.com>
2021-03-30 22:33:12 -0700

    finsihed projects part

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
+$(window).on('load', function(){
+    $('.loader .inner').fadeOut(800, function(){
+        $('.loader').fadeOut(800);
+    });
+    $('.items').isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 1500,
+            easing: 'linear',
+            queue: false
+        }
+    });
+})
 $(() => {
 
 $('#slides').superslides({ 
     animation: 'fade',
-    play: 4000,
+    play: 4700,
     pagination: false,
     animation_easing: 'swing'
 })
@@ -12,15 +25,19 @@ let typed = new Typed(".typed", {
     typeSpeed: 80,
     loop: true,
     fadeOut: true,
-    fadeOutDelay: 1500,
+    fadeOutDelay: 1700,
     startDelay: 1000,
     showCursor: false
 });
 
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    items: 4,
-    responsive:{
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 1,
+    autoplay: 1000,
+    autoplaySpeed:500,
+    dots: true,
+    responsive: {
         0:{
             items:1
         },
@@ -35,7 +52,6 @@ $('.owl-carousel').owlCarousel({
         }
     }
 });
-
 
 let skillsTopOffset = $('.skillsSection').offset().top;
 let statsTopOffset = $('.statsSection').offset().top;
@@ -65,6 +81,44 @@ $(window).scroll(function() {
     }
 });
 
+$('[data-fancybox]').fancybox();
 
+
+    $('#filters a').click(function(){
+        $('#filters .current').removeClass('current');
+        $(this).addClass('current');
+        let selector = $(this).attr('data-filter');
+        $('.items').isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+$('#navigation li a').click(function(e) {
+    e.preventDefault();
+    let targetElement = $(this).attr('href');
+    let targetPosition = $(targetElement).offset().top;
+    $('html, body').animate({
+        scrollTop: targetPosition - 70
+    }, 'slow');
+
+});
+
+const nav = $('#navigation');
+const navTop = nav.offset().top;
+$(window).on('scroll', stickyNavigation);
+function stickyNavigation() {
+    let body = $('body');
+    if($(window).scrollTop() >= navTop) {
+        body.css('padding-top', nav.outerHeight() + 'px');
+        body.addClass('fixedNav');
+    }else {
+        body.removeClass('fixedNav');
+    }
+}
 
 });
